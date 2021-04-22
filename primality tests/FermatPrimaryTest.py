8fed38adae6c34abfee8bbfbf1324ace8c3f91b9
b-garbacz <bartigarbacz@gmail.com>
2021-04-22 02:11:06 +0200

    Fermat primary test

--- primality tests/FermatPrimaryTest.py
+++ primality tests/FermatPrimaryTest.py
@@ -1,3 +1,12 @@
+def fast_p(a, b, n) -> object:
+    e = dec2bin(b)
+    c = 1
+    for i in range(0, len(e)):
+        c = (c ** 2) % n
+        if e[i] == 1:
+            c = (c * a) % n
+    return c
+
 def fermat(n, k) -> bool:
     if n % 2 == 0:
         return False
